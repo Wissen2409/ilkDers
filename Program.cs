a88ddd0430e6af7645fe3dbaa20825bad6afe5b0
emrahelis <emrahelis@gmail.com>
2024-09-04 21:37:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bugün istanbulda yağmur var");
+
+//Console.WriteLine("Bugün güneşli");
+// yukarıdaki kod bloğu şu işi yapmaktadır. Sakın dokunma
+
+// yorum satırı denir ve yorum satırında olan kodlar, asla derleyici tarafından derlenmez ve çalıştırılmaz.
+
+
+Console.writeLine("bugün hava karlı");
